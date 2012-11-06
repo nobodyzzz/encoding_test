58a0d10797872d391a1f4940718dd84658f7ceb6
Stas Turlo <stanislav.turlo@rightscale.com>
2012-11-06 17:08:27 +0300

    acu60206 Create middleware for sinatra to provide default charset and/or transcode into charset

--- config/skeletor_init.rb
+++ config/skeletor_init.rb
@@ -1,2 +1,31 @@
 # Global Skeletor params
 Skeletor::Deployer.setup(File.expand_path("../../", __FILE__), RACK_ENV)
+
+module Skeletor
+  module Middleware
+    class PostBodyContentTypeParser
+      alias_method :call_original, :call
+
+      def can_convert?(charset)
+        begin
+          Encoding::find(charset)
+          true
+        rescue ArgumentError
+          false
+        end
+      end
+
+      def call(env)
+        request = Rack::Request.new(env)
+        unless request.content_charset == "utf-8"
+          if can_convert? request.content_charset
+            s = env[POST_BODY].read
+            s.encode!("utf-8", request.content_charset)
+            env[POST_BODY] = StringIO.new(CGI.escape(s).sub("%3D", "="))
+          end
+        end
+        call_original(env)
+      end
+    end
+  end
+end
